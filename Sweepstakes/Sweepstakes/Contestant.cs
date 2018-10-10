382bbe661327cabbcc1868b9c1b5d4e450273825
imilian007 <43039654+imilian007@users.noreply.github.com>
2018-10-10 08:19:10 -0500

    Initial commit with contestant class

---
+++ Sweepstakes/Sweepstakes/Contestant.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Sweepstakes
+{
+    public class Contestant
+    {
+        string firstName;
+        string lastName;
+        string emailAddress;
+        double registrationNumber;
+        public Contestant()
+        {
+        }
+    }
+}
