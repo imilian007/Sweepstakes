6b6032346c3359029d29a47b35fa8ffd89da7eaa
imilian007 <43039654+imilian007@users.noreply.github.com>
2018-10-10 14:29:00 -0500

    Instantiated contestant dictionary

--- Sweepstakes/Sweepstakes/Contestant.cs
+++ Sweepstakes/Sweepstakes/Contestant.cs
@@ -3,12 +3,41 @@ namespace Sweepstakes
 {
     public class Contestant
     {
-        string firstName;
-        string lastName;
-        string emailAddress;
-        double registrationNumber;
+        public string firstName;
+        public string lastName;
+        public string emailAddress;
+        public double registrationNumber;
+
         public Contestant()
         {
+        }
+
+        private string GetInfoString(string message)
+        {
+            string infoString;
+            Console.WriteLine(message);
+            infoString = Console.ReadLine();
+            return infoString;
+        }
+
+        private int GetInfoInt(string message)
+        {
+            int infoInt;
+            Console.WriteLine(message);
+            infoInt = int.Parse(Console.ReadLine());
+            return infoInt;
+        }
+
+        public string ContestantInfoString(string message)
+        {
+            string infoString = GetInfoString(message);
+            return infoString;
+        }
+
+        public int ContestantInfoInt(string message)
+        {
+            int infoInt = GetInfoInt(message);
+            return infoInt;
         }
     }
 }
