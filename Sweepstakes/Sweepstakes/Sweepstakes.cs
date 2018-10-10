3fe7b465b19ff970fdc33b3fabc2935817a9beed
imilian007 <43039654+imilian007@users.noreply.github.com>
2018-10-10 08:23:13 -0500

    Implemented Sweepstakes class skeleton

---
+++ Sweepstakes/Sweepstakes/Sweepstakes.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace Sweepstakes
+{
+    public class Sweepstakes
+    {
+        public Sweepstakes()
+        {
+        }
+
+        public Sweepstakes(string name)
+        {
+
+        }
+
+        public void RegisterContestant(Contestant contestant)
+        {
+
+        }
+
+        public string PickWinner()
+        {
+
+        }
+
+        public void PrintContestantInfo(Contestant contestant)
+        {
+
+        }
+    }
+}
